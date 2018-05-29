99a15bf60c0e7592bb5c33e47313492e8bb44fb2
eduardokruel <32836018+eduardokruel@users.noreply.github.com>
2018-05-29 11:46:35 -0300

    Added Scroll Reveal

--- js.js
+++ js.js
@@ -2,3 +2,9 @@ window.onload = function () {
   var h = window.innerHeight - 50 + "px";
 	document.getElementById("interpreter").height = h;
 };
+
+window.sr = ScrollReveal();
+sr.reveal("p");
+sr.reveal("h3");
+sr.reveal(".example");
+sr.reveal("ul");
